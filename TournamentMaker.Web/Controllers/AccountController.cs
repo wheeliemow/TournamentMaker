4034d4d6814e9a60c2cada51c72edc5fa713c671
Haacked <haacked@gmail.com>
2012-07-31 12:05:59 -0700

    Added thanks to AppHarbor

--- TournamentMaker.Web/Controllers/AccountController.cs
+++ TournamentMaker.Web/Controllers/AccountController.cs
@@ -1,158 +1,154 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Security.Principal;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
 using TournamentReport.Models;
 using TournamentReport.Services;
 
 namespace TournamentReport.Controllers
 {
-	public class AccountController : Controller
-	{
-		public IWebSecurityService WebSecurityService { get; set; }
+    public class AccountController : Controller
+    {
+        public IWebSecurityService WebSecurityService { get; set; }
 
-		protected override void Initialize(RequestContext requestContext)
-		{
-			if (WebSecurityService == null) { WebSecurityService = new WebSecurityService(); }
+        protected override void Initialize(RequestContext requestContext)
+        {
+            if (WebSecurityService == null)
+            {
+                WebSecurityService = new WebSecurityService();
+            }
 
-			base.Initialize(requestContext);
-		}
+            base.Initialize(requestContext);
+        }
 
-		// **************************************
-		// URL: /Account/LogOn
-		// **************************************
+        // **************************************
+        // URL: /Account/LogOn
+        // **************************************
 
-		public ActionResult LogOn()
-		{
-			return View();
-		}
+        public ActionResult LogOn()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public ActionResult LogOn(LogOnModel model, string returnUrl)
-		{
-			if (ModelState.IsValid)
-			{
-				if (WebSecurityService.Login(model.UserName, model.Password, model.RememberMe))
-				{
-					if (Url.IsLocalUrl(returnUrl))
-					{
-						return Redirect(returnUrl);
-					}
-					else
-					{
-						return RedirectToAction("Index", "Home");
-					}
-				}
-				else
-				{
-					ModelState.AddModelError("", "The user name or password provided is incorrect.");
-				}
-			}
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (WebSecurityService.Login(model.UserName, model.Password, model.RememberMe))
+                {
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
 
-			// If we got this far, something failed, redisplay form
-			return View(model);
-		}
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
 
-		// **************************************
-		// URL: /Account/LogOff
-		// **************************************
+        // **************************************
+        // URL: /Account/LogOff
+        // **************************************
 
-		public ActionResult LogOff()
-		{
-			WebSecurityService.Logout();
+        public ActionResult LogOff()
+        {
+            WebSecurityService.Logout();
 
-			return RedirectToAction("Index", "Home");
-		}
+            return RedirectToAction("Index", "Home");
+        }
 
-		// **************************************
-		// URL: /Account/Register
-		// **************************************
+        // **************************************
+        // URL: /Account/Register
+        // **************************************
 
-		public ActionResult Register()
-		{
-			ViewBag.PasswordLength = WebSecurityService.MinPasswordLength;
-			return View();
-		}
+        public ActionResult Register()
+        {
+            ViewBag.PasswordLength = WebSecurityService.MinPasswordLength;
+            return View();
+        }
 
-		[HttpPost]
-		public ActionResult Register(RegisterModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				// Attempt to register the user
-				var requireEmailConfirmation = false;
-				var token = WebSecurityService.CreateUserAndAccount(model.UserName, model.Password, requireEmailConfirmation);
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Attempt to register the user
+                var requireEmailConfirmation = false;
+                var token = WebSecurityService.CreateUserAndAccount(model.UserName, model.Password,
+                                                                    requireEmailConfirmation);
 
-				if (requireEmailConfirmation)
-				{
-					// TODO: Send email to user with confirmation token
+                if (requireEmailConfirmation)
+                {
+                    // TODO: Send email to user with confirmation token
 
-					// Thank the user for registering and let them know an email is on its way
-					return RedirectToAction("Thanks", "Account");
-				}
-				else
-				{
-					// Navigate back to the homepage and exit
-					WebSecurityService.Login(model.UserName, model.Password);
-					return RedirectToAction("Index", "Home");
-				}
-			}
+                    // Thank the user for registering and let them know an email is on its way
+                    return RedirectToAction("Thanks", "Account");
+                }
+                else
+                {
+                    // Navigate back to the homepage and exit
+                    WebSecurityService.Login(model.UserName, model.Password);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
 
-			// If we got this far, something failed, redisplay form
-			ViewBag.PasswordLength = WebSecurityService.MinPasswordLength;
-			return View(model);
-		}
+            // If we got this far, something failed, redisplay form
+            ViewBag.PasswordLength = WebSecurityService.MinPasswordLength;
+            return View(model);
+        }
 
-		// **************************************
-		// URL: /Account/ChangePassword
-		// **************************************
+        // **************************************
+        // URL: /Account/ChangePassword
+        // **************************************
 
-		[Authorize]
-		public ActionResult ChangePassword()
-		{
-			ViewBag.PasswordLength = WebSecurityService.MinPasswordLength;
-			return View();
-		}
+        [Authorize]
+        public ActionResult ChangePassword()
+        {
+            ViewBag.PasswordLength = WebSecurityService.MinPasswordLength;
+            return View();
+        }
 
-		[Authorize]
-		[HttpPost]
-		public ActionResult ChangePassword(ChangePasswordModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				if (WebSecurityService.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword))
-				{
-					return RedirectToAction("ChangePasswordSuccess");
-				}
-				else
-				{
-					ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
-				}
-			}
+        [Authorize]
+        [HttpPost]
+        public ActionResult ChangePassword(ChangePasswordModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (WebSecurityService.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword))
+                {
+                    return RedirectToAction("ChangePasswordSuccess");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
+                }
+            }
 
-			// If we got this far, something failed, redisplay form
-			ViewBag.PasswordLength = WebSecurityService.MinPasswordLength;
-			return View(model);
-		}
+            // If we got this far, something failed, redisplay form
+            ViewBag.PasswordLength = WebSecurityService.MinPasswordLength;
+            return View(model);
+        }
 
-		// **************************************
-		// URL: /Account/ChangePasswordSuccess
-		// **************************************
+        // **************************************
+        // URL: /Account/ChangePasswordSuccess
+        // **************************************
 
-		public ActionResult ChangePasswordSuccess()
-		{
-			return View();
-		}
+        public ActionResult ChangePasswordSuccess()
+        {
+            return View();
+        }
 
-		public ActionResult Thanks()
-		{
-			return View();
-		}
-
-	}
+        public ActionResult Thanks()
+        {
+            return View();
+        }
+    }
 }
